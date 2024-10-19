75abdaf033e5c2d2103d6d62f1dfd505f46d9417
khadir <tolibxonovm@gmail.com>
2024-10-19 12:28:08 +0500

    +

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-// import clubs from "./data.js";
-
-// const allClubs = document.querySelector("ul");
-// clubs.forEach((club) => {
-//   allClubs.innerHTML += `
-//     <li class="all-clubs">
-//         <div class="club-title-logo">
-//           <img src=${club.image} width="40">
-//         </div>
-//         <h3 class="team">  ${club.name}</h3>
-//         <p class="text"> ${club.history}</p>
-//         <ul class="all-trophies">
-//             ${Object.entries(club.trophies).map((trophe, count) => {
-//               return `<li class="trophies">
-//               ${trophe}-${count}
-//                 </li>`;
-//             })}
-//         </ul>
-
-//     </li>
-
-//    `;
-// });
 import clubs from "./data.js";
 
 const allClubs = document.querySelector("ul");
